a9ce62bcd41be03d2789a5707da6955fd4b037f5
siva <mylasivakumar9@gmail.com>
2026-04-19 12:22:16 +0200

    r.py

--- 1.py
+++ 1.py
@@ -1,4 +1,28 @@
-df = spark.read.csv("location")
-df.display()
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
+def cx():
+    for i in data:
+        if i > 2000 and i is int:
+            print("good")
+        if i < 2000: 
+            print("not godd")
+        else:
+            print("np")
+
+
+data = [2000,888787,999999]
+
+
+a = cx()
+print(a)
+
+
+data = "mylasivakumar9@gmail.com"
+mask = data.split("@")
+print((mask)[0])
+print((mask)[-1])
+
+class swedbank:
+
+    def __init__(self,tech,domain):
+        self.tech = tech
+        self.domain = domain
+    def fx1
--- 2.py
+++ 2.py
@@ -1,5 +1,27 @@
-df1 = df.select("salaty").count()
-df1.display()
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("My First PySpark App") \
+    .getOrCreate()
+
+data = [
+    ("Siva", 25, 5000),
+    ("Ravi", 30, 6000),
+    ("Kumar", 35, None)
+]
+
+columns = ["Name", "Age", "Salary"]
+
+df = spark.createDataFrame(data, columns)
+
+# Count example
+df1 = df.select("Salary").count()
+
+print("Count:", df1)
+
+# Show DataFrame
+df.show()
+
+spark.stop()
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
---- test---
